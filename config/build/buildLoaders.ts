047f4021b72718f5335171d86a82e680372c7592
justkutmans <justkutmans@gmail.com>
2023-06-14 16:48:24 +0600

    update gitignore

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,31 @@
+import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
+
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+    const styleLoaders =        {
+            test: /\.s[ac]ss$/i,
+            use: [
+                options.isDev ? 'style-loader' :  MiniCssExtractPlugin.loader,
+                {
+                    loader: "css-loader",
+                    options: {
+                        modules: {
+                            auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                            localIdentName: options.isDev ? "[path][name]__[local]--[hash:base64:5]": "[hash:base64:8]"
+                        }
+                    },
+                },
+                "sass-loader",
+            ],
+        }
+        const typeScriptLoaders =    {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            }
+    return  [
+        styleLoaders,
+        typeScriptLoaders
+    ]
+}
